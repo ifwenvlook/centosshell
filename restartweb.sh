99e3eb256bf857bbf484190bf031cd9420c451e6
wlz_centos <wlz@wenlz.top>
2018-05-23 09:51:14 -0400

    20180523  restartweb nginx.conf

--- restartweb.sh
+++ restartweb.sh
@@ -1,14 +1,20 @@
 #!/bin/sh -x
 
 service mysqld restart
-service nginx  restart  || /usr/bin/nginx 
+sleep 3
+killall -9 nginx
+sleep 3
 service redis stop
 sleep 3
 killall -9 uwsgi
-/usr/local/bin/uwsgi /home/blog/config.ini -d /home/blog/uwsgi.log
+sleep 3
 killall -9 celery
 sleep 3
+/usr/local/bin/uwsgi /home/blog/config.ini -d /home/blog/uwsgi.log
+sleep 1
 service redis start
-sleep 3
+sleep 1
+/usr/bin/nginx
+sleep 1
 cd /home/blog
 celery worker -A celery_worker.celery -l INFO &
